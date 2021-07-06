eb2dbed708904c178a86769f874a7beab5e022ed
Kurt Heiden <kurt.w.heiden@gmail.com>
2021-07-06 13:19:50 -0700

    Update server.py

--- local/server.py
+++ local/server.py
@@ -21,7 +21,7 @@ class Server():
 
     @app.route("/v2/move", methods=['POST'])
     def move():
-      data = request.get_json()
+      data = request.json
       axis_name = data['axis_name']
       axis_value = data['axis_value']
       controller_type = data['controller_type']
