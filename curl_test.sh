99b6128a1ae1480ab18b30ee9223fccee4a90ddd
Kurt Heiden <kurt.w.heiden@gmail.com>
2021-07-06 14:05:52 -0700

    Adding helper scripts
    - to test the API
    - to reload the service

---
+++ curl_test.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'axis_name=Axis 0&axis_value=0.0&controller_type=1'  http://<IP_ADDR>:<PORT>/v2/move
---
+++ service_reload.sh
@@ -0,0 +1,3 @@
+sudo systemctl daemon-reload
+sudo systemctl restart Web_Controller.service
+sudo systemctl status restart Web_Controller.service
