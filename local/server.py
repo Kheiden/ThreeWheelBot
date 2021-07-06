ffb1f200dbff5cc9bdc76aae2de635473fe7ef8f
Kurt Heiden <kurt.w.heiden@gmail.com>
2021-07-06 13:22:06 -0700

    Update server.py

--- local/server.py
+++ local/server.py
@@ -21,17 +21,18 @@ class Server():
 
     @app.route("/v2/move", methods=['POST'])
     def move():
-      data = request.json
-      print(data)
-      axis_name = data['axis_name']
-      axis_value = data['axis_value']
-      controller_type = data['controller_type']
+      output = request.json
 
-      output = self.m.move_robot(
-        axis_name=axis_name,
-        axis_value=float(axis_value),
-        controller_type=controller_type)
       return Response(output, mimetype='text/HTML')
+      # axis_name = data['axis_name']
+      # axis_value = data['axis_value']
+      # controller_type = data['controller_type']
+      #
+      # output = self.m.move_robot(
+      #   axis_name=axis_name,
+      #   axis_value=float(axis_value),
+      #   controller_type=controller_type)
+      # return Response(output, mimetype='text/HTML')
 
     return app
 
